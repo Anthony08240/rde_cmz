e3ef7b20d5a78770a7993c7391d6d954d38ffbc9
Anthony08240 <beenen@simplon-charleville.fr>
2020-09-17 15:57:03 +0200

    v-test2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <!-- Revolution includes -->
 <?php include('include/revolution_head.php'); ?>
 
-<!-- Bootstrap CSS -->
+<!-- Bootstrap CSS -->fegrth
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
   integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="assets/css/style.css">
